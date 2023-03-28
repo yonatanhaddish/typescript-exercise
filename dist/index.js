699d0cbd6fad504fa74453b55ced7d1b430fcb01
Yonatan Haddish <yonihailshn@gmail.com>
2023-03-28 17:05:47 -0400

    typescripts primitives

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,12 @@
 "use strict";
-console.log("Hello World!");
-let age = 20;
+let sales = 123456789;
+let sales02 = 123456789;
+let course = "TypeScript";
+let course02 = "TypeScript";
+let is_published = true;
+let is_published02 = true;
+let level;
+let numbers = [1, 2, 3];
+numbers.forEach(n => console.log(n.toExponential));
+let user = [1, 'Yoni'];
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
-let sales: number = 123_456_789;
+// -------------------------------------------------------------
+// primitives
+// -------------------------------------------------------------
 let sales02 = 123_456_789;
 
-let course: String = "TypeScript";
+let course: string = "TypeScript";
 let course02 = "TypeScript";
 
 let is_published: boolean = true;
 let is_published02 = true;
 
 let level;
+// -------------------------------------------------------------
+// arrays
+// -------------------------------------------------------------
+let numbers: number[] = [1, 2, 3];
+numbers.forEach(n => console.log(n.toExponential))
+// -------------------------------------------------------------
+// tuples
+// -------------------------------------------------------------
+let user: [number, string] = [1, 'Yoni'];
